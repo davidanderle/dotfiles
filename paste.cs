4cf514b05baac3263acfac31ce629e14a0443de1
davidanderle <anderle.david@gmail.com>
2018-07-10 15:43:03 +0200

    paste.exe can now handle UTF-8 chars.

--- paste.cs
+++ paste.cs
@@ -8,11 +8,13 @@
 using System;
 using System.Windows.Forms;
 using System.Threading;
+using System.Text;
 
 public class Paste {
 	[STAThread]
 	static void Main( string[] args )
 	{
+		Console.OutputEncoding = Encoding.UTF8;
 		foreach(string line in Clipboard.GetText().Split(new string[]{"\r\n","\n","\r"}, StringSplitOptions.None ))
 			Console.Write(line+"\n");
 	}
