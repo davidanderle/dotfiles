390bf09eae275edded8b8b2e7d61108986904495
davidanderle <anderle.david@gmail.com>
2018-07-12 12:22:14 +0200

    Addint ctags and its config

--- .autosetup.sh
+++ .autosetup.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 USERNAME=$(anderle)
 PACKAGE_LIST="
-biber bsdmainutils bsdutils btrfs-tools build-essential bzip2 ca-certificates
-cmake cu curl diffutils dpkg dump findutils gcc-6-base gcc-arm-none-eabi gdb
-gdb-arm-none-eabi gdisk gedit git git-man grep gzip less linux-tools-common
-neovim parted passwd pastebinit python python3 python3.5 python3.5-minimal
-python3-apport python3-apt python3-blinker python3-cffi-backend
-python3-chardet python3-commandnotfound python3-configobj python3-cryptography
-python3-dbus python3-debian python3-dev python3-distupgrade python3-gdbm
-python3-gi python3-idna python3-jinja2 python3-jsonpatch python3-json-pointer
-python3-jwt python3-markupsafe python3-minimal python3-newt python3-oauthlib
-python3-pkg-resources python3-prettytable python3-problem-report
-python3-pyasn1 python3-pycurl python3-requests python3-serial python3-six
-python3-software-properties python3-systemd python3-update-manager
-python3-urllib3 python3-yaml python-apt-common python-dev python-pip screen
-sed tar texlive-latex-base texlive-bibtex-extra texlive-math-extra
-texlive-xetex tmux valgrind wget xclip zip"
+exuberant-ctags biber bsdmainutils bsdutils btrfs-tools build-essential bzip2
+ca-certificates cmake cu curl diffutils dpkg dump findutils gcc-6-base
+gcc-arm-none-eabi gdb gdb-arm-none-eabi gdisk gedit git git-man grep gzip less
+linux-tools-common neovim parted passwd pastebinit python python3 python3.5
+python3.5-minimal python3-apport python3-apt python3-blinker
+python3-cffi-backend python3-chardet python3-commandnotfound python3-configobj
+python3-cryptography python3-dbus python3-debian python3-dev
+python3-distupgrade python3-gdbm python3-gi python3-idna python3-jinja2
+python3-jsonpatch python3-json-pointer python3-jwt python3-markupsafe
+python3-minimal python3-newt python3-oauthlib python3-pkg-resources
+python3-prettytable python3-problem-report python3-pyasn1 python3-pycurl
+python3-requests python3-serial python3-six python3-software-properties
+python3-systemd python3-update-manager python3-urllib3 python3-yaml
+python-apt-common python-dev python-pip screen sed tar texlive-latex-base
+texlive-bibtex-extra texlive-math-extra texlive-xetex tmux valgrind wget xclip
+zip"
 
 echo "Entering root directory..."
 cd ~/
